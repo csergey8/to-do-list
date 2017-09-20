8d1fb35b148f0b19b8e04a2deab57f9ad0e5776c
csergey8 <c.sergey@protonmail.com>
2017-09-20 15:45:17 +0300

    add events on parents elements (for new elements)

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,13 +1,20 @@
-$("li").click(function () {
+$("ul").on("click", "li", function () {
 
 
     $(this).toggleClass("completed");
 
 });
 
-$("span").click(function (e) {
+$("ul").on("click", "span", function (e) {
     e.stopPropagation();
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
+});
+
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $(this).val("");
+    };
 });
