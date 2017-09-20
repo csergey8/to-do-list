c0665ee18d0152345b418f4d3134ac534174b315
csergey8 <c.sergey@protonmail.com>
2017-09-20 15:14:40 +0300

    add delet code item

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -3,19 +3,11 @@ $("li").click(function () {
 
     $(this).toggleClass("completed");
 
+});
 
-
-    /*
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
-    } else {
-        $(this).css({
-            color: "grey",
-            textDecoration: "line-through"
-        });
-    };
-*/
+$("span").click(function (e) {
+    e.stopPropagation();
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
 });
